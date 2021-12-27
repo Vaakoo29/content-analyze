c5afded37e1dac3e13be52e18e47868957149b98
Sara Ganbat <email@example.com>
2021-12-27 23:39:52 +0700

    adding

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print('ей, ты меня скопировал')
-print('у тебя получилось изменить меня...)')
+from tika import parser 
+
+raw = parser.from_file('strategies/Strategia_Danii.pdf')
+
+content = raw['content']
+
+count = 0
+
+content = content.split()
+
+for i in content:
+    if i == 'ecology':
+        count+=1
+
+print(count)
