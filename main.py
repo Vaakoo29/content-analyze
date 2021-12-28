ce5784595d4716fefecf1e41c3a481a6ac38c019
Sara Ganbat <email@example.com>
2021-12-28 12:35:34 +0700

    rabotenka

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from tika import parser 
+from tika import parser
+import pandas as pd
 
-raw = parser.from_file('strategies/Strategia_Danii.pdf')
+# Словари для проверки определенных тендеций
+economics = ['business', 'economic', 'economics', 'development', 'resources', 'exploitation', 'project', 'projects']
+humans = ['human', 'humans', 'indigenous', 'people', 'peoples', 'rights', 'nation', 'nations', 'folk', 'folks', 'social', 'welfare', 'safety', 'education', 'educational']
+politics = ['politics', 'cooperation', 'international', 'political', 'policy', 'management']
+ecology = ['ecology', 'pollution', 'environment', 'climate', 'security', 'safety', 'science']
 
-content = raw['content']
+tendencies = [economics, humans, politics, ecology]
 
-count = 0
+# Страны
+countries = {'китай':'china', 'дания':'denmark', 'канада': 'canada', 'финляндия':'finland', 'iceland':'исландия', 'норвегия':'norway', 'швеция':'sweden', 'сша':'usa', 'россия':'russia'}
 
-content = content.split()
+print("Cтраны, входящие в Арктический Союз: Канада, Китай, Дания, Финляндия, Исландия, Норвегия, Швеция, США, Россия")
+country = input("Выберите страну: ")
+country = countries[country.lower()]
 
-for i in content:
-    if i == 'ecology':
-        count+=1
+raw = parser.from_file('strategies/' + country +'.pdf')
+content = raw['content'].split()
 
-print(count)
+df_columns = ['word', 'frequency']
+rows = []
+
+for tendency in tendencies:
+    for word in tendency:
+        rows.append({'word': word, 'frequency': content.count(word)})
+    
+    print(tendency)
+    out_df = pd.DataFrame(rows, columns=df_columns)
+    print(out_df)
